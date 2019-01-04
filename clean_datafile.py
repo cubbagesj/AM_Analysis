464b50016d1d21a1ec715996fe0f3151a41a270f
scubbage <samuel.cubbage@navy.mil>
2019-01-04 13:39:47 -0500

    File cleaning tool now working

--- clean_datafile.py
+++ clean_datafile.py
@@ -24,15 +24,18 @@ the name of the file if required.
 The tool creates an entirely new file instead of modifying and existing one to 
 be sure that no data is hidden in the slack space.
 """
-import os, glob
+import os
 import wx
+
+import plottools as plottools
 
 class CleanDataFrame(wx.Frame):
     
     def __init__(self, paths ):
         '''Initializes the frame and all the widgets.  
         '''
-        wx.Frame.__init__(self, None, -1, 'Clean Datafile', style=wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
+        wx.Frame.__init__(self, None, -1, 'Datafile Cleaning Tool', 
+                          style=wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
         self.SetBackgroundColour(wx.Colour("LIGHTGREY"))
         
         # Setup status bar
@@ -44,120 +47,175 @@ class CleanDataFrame(wx.Frame):
         self.runsToMerge = []
 
         # decorate the frame with the widgets
-        topLbl = wx.StaticText(self, -1, "Merge Runs")
-        topLbl.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
+        topLbl = wx.StaticText(self, -1, "Select File To Clean")
+        topLbl.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
         
-        # Merge input file section
-        fileLabel = wx.StaticText(self, -1, "Merge Input File:")
+        # Input file section
         self.fileText = wx.TextCtrl(self, -1, value="", 
-                                    size=(200, -1))
+                                    size=(400, -1))
         self.fileBtn = wx.Button(self, -1, "Pick File")
         self.Bind(wx.EVT_BUTTON, self.OnFileClick, self.fileBtn)
 
-        runLabel = wx.StaticText(self, -1, "Select Files to Merge")
-        runLabel.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        # File display section
+        displayLabel = wx.StaticText(self, -1, "File Information")
+        displayLabel.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
 
-        stdLabel = wx.StaticText(self, -1, "Select STD Location")
-        stdLabel.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.headerText = wx.TextCtrl(self, -1, "", size=(200,100),
+                                      style=wx.TE_MULTILINE|wx.TE_PROCESS_ENTER)
         
-        # File selector
-        self.dirpick = wx.DirPickerCtrl(self, -1, 
-                                        path=self.defaultPaths['obcDir'], 
-                                        message="Choose Directory")
-        self.fileList = wx.CheckListBox(self, -1, 
-                                        size=(200,150), 
-                                        style=wx.LB_MULTIPLE)
-
-        self.Bind(wx.EVT_DIRPICKER_CHANGED,  self.OnDirPick, self.dirpick)
-        self.Bind(wx.EVT_CHECKLISTBOX, self.OnCheck, self.fileList) #AFP Updated onCheck function
-
-
-        self.mrgdirpick = wx.DirPickerCtrl(self, -1, 
-                                           path=self.defaultPaths['stdDir'], 
-                                           message="Choose Directory",
-                                           style=wx.DIRP_USE_TEXTCTRL)
-        self.Bind(wx.EVT_DIRPICKER_CHANGED, self.OnMrgDirPick, self.mrgdirpick)
  
         # Run Merge Button
-        mergeBtn = wx.Button(self, -1, "Run Merge")
-        mergeBtn.SetFont(wx.Font(16, wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.Bind(wx.EVT_BUTTON, self.OnMergeClick, mergeBtn)
+        cleanBtn = wx.Button(self, -1, "Clean File")
+        cleanBtn.SetFont(wx.Font(16, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.Bind(wx.EVT_BUTTON, self.OnCleanClick, cleanBtn)
 
         # Set up the layout with sizers
+        
+        # Main Title
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         mainSizer.Add(topLbl, 0, wx.ALL, 5)
-        mainSizer.Add(wx.StaticLine(self), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5)
         
+        # File selection section
         fileSizer = wx.BoxSizer(wx.HORIZONTAL)
-        fileSizer.Add(fileLabel, 0, wx.ALL, 5)
-        fileSizer.Add(self.fileText, 0, wx.ALL, 5)
         fileSizer.Add(self.fileBtn, 0, wx.ALL, 5)
+        fileSizer.Add(self.fileText, 0, wx.ALL, 5)
         mainSizer.Add(fileSizer, 0, wx.EXPAND|wx.ALL, 10)
 
-        mainSizer.Add(runLabel, 0, wx.EXPAND|wx.ALL, 10)
+        mainSizer.Add(wx.StaticLine(self), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5)
 
-        fileSizer2 = wx.FlexGridSizer(cols=1, hgap=5, vgap=5)
-        fileSizer2.AddGrowableCol(0)
-        fileSizer2.Add(self.dirpick, 0, wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
-        fileSizer2.Add(self.fileList, 0, wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
-        fileSizer2.Add(stdLabel, 0, wx.EXPAND|wx.ALL, 10)
-        fileSizer2.Add(self.mrgdirpick, 0, wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
-        mainSizer.Add(fileSizer2, 0, wx.EXPAND|wx.ALL, 10)
+        mainSizer.Add(displayLabel, 0, wx.EXPAND|wx.ALL, 10)
+        mainSizer.Add(self.headerText, 0, wx.EXPAND|wx.ALL,10)
+        
+        #Setup for channel entry
+        chanLbl = wx.StaticText(self, -1, "Channel")
+        self.chanList = wx.ListBox(self, -1, 
+                                   style=wx.LB_SINGLE, name="Channel")
+        nameLbl = wx.StaticText(self, -1, "New Name")
+        self.newName = wx.TextCtrl(self, -1, "")
+        
+        scaleLbl = wx.StaticText(self, -1, "Scale")
+        self.newScale = wx.TextCtrl(self, -1, "1.0")
+
+        self.mapBtn = wx.Button(self, -1, "Add Channel")
+        self.Bind(wx.EVT_BUTTON, self.OnAddClick, self.mapBtn)
+
+        #Setup for Mapping info
+        self.mapInfo = wx.TextCtrl(self, -1, "", size=(300, 300),
+                                   style=wx.TE_MULTILINE|wx.TE_PROCESS_ENTER )
+        
+        # Sizer for channel Input
+        chanSizer = wx.FlexGridSizer(cols=2, hgap=5, vgap=5)
+        chanSizer.Add(chanLbl, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
+        chanSizer.Add(self.chanList, 0)
+        chanSizer.Add(nameLbl, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
+        chanSizer.Add(self.newName, 0, wx.EXPAND)
+        chanSizer.Add(scaleLbl, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
+        chanSizer.Add(self.newScale, 0, wx.EXPAND)
+        
+        # Sizer for Channel Mapping
+        mapSizer = wx.StaticBoxSizer(wx.VERTICAL, self, label='Output Map')
+        mapSizer.Add(self.mapInfo, 0, wx.EXPAND)
+        
+        
+        # Create sizer for channel information
+        configSizer = wx.StaticBoxSizer(wx.VERTICAL,self, label='Channel Input')
+        configSizer.Add(chanSizer, 0, wx.LEFT)
+        configSizer.Add(self.mapBtn, 0, wx.EXPAND)
+        
+        groupSizer = wx.BoxSizer(wx.HORIZONTAL)
+        groupSizer.Add(configSizer, 0, wx.LEFT, 5)
+        groupSizer.Add(mapSizer, 0, wx.RIGHT, 5)
+              
+        mainSizer.Add(groupSizer, 0, wx.LEFT, 5)
 
         mainSizer.Add((20,20), 0)
        
         mainSizer.Add(wx.StaticLine(self), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5)
-        mainSizer.Add(mergeBtn, 0, wx.EXPAND|wx.ALL, 5)
+        mainSizer.Add(cleanBtn, 0, wx.EXPAND|wx.ALL, 5)
         
         self.SetSizer(mainSizer)
         self.Fit()
         
 
     def OnFileClick(self, evt):
-        dlg = wx.FileDialog(self, "Open merge input file...",
-                                defaultDir="/home/pi/Documents/Merge_Files", wildcard="*.*",
+        dlg = wx.FileDialog(self, "Choose file to clean...",
+                                defaultDir=".", wildcard="*.*",
                                 style=wx.FD_OPEN)
         if dlg.ShowModal() == wx.ID_OK:
-            self.MergeCfg = dlg.GetPath()
+            self.inputFile = dlg.GetPath()
             self.fileText.SetValue(dlg.GetFilename())
         dlg.Destroy()
-    
         
-    def OnMergeClick(self, evt):
+        # Clear the current header info
+        self.headerText.ChangeValue("")
+        # Open the file and read in the header info
+        f = open(self.inputFile, 'r')
+        for x in range(4):
+            self.headerText.write(f.readline())
+        f.close()
+        
+        # Now pull in the whole file
+        self.runObj = plottools.get_run(self.inputFile)
+        
+        self.chanList.Set([])
+        self.chanList.InsertItems(self.runObj.chan_names, 0)
+        self.chanList.SetSelection(0)
+        
+    def OnCleanClick(self, evt):
         """
             Get the values from the dialog box and create the plot
             with a call to mplt in the plottools module
         """
-        pass
+        # Start by getting the mapping info
         
-    def OnDirPick(self, evt):
-        self.dataDir = self.dirpick.GetPath()
-        self.fileList.Set(self.getFiles())
+        oldNames = []
+        newNames = []
+        newScales = []
         
-    def OnCheck(self, evt):
-        self.runsToMerge = []
-        for item in range(self.fileList.GetCount()):  
-            if self.fileList.IsChecked(item): 
-                try:
-                    self.runsToMerge.index(self.fileList.GetString(item))
-                except:
-                    self.runsToMerge.append(self.fileList.GetString(item))
-            else: #AFP Remove the item from fileList if it is unchecked
-                try:
-                    self.runsToMerge.remove(self.fileList.GetString(item))
-                except:
-                    continue
-                  
-    def getFiles(self):
-        datafiles = [] # now also includes TDMS files
+        for i in range(self.mapInfo.GetNumberOfLines()):
+            try:
+                oldName, newName, newScale = self.mapInfo.GetLineText(i).split(',')
+            except:
+                pass
+            oldNames.append(oldName)
+            newNames.append(newName)
+            newScales.append(newScale)
+            
+        # Now open an output file - append 'clean' to original
+        cleanFile = open(self.inputFile + 'clean', 'w')
+        
+        # Write the header info
+        cleanFile.write(self.headerText.GetValue() + '\n')
+        
+        # Now apply the mapping
+        for channel in self.runObj.chan_names:
+            try:
+                idx = oldNames.index(channel)
+                # Channel in map list so process
+                self.runObj.dataEU[channel] *= float(newScales[idx])
+                cleanFile.write("%s " % newNames[idx])
+            except:
+                self.runObj.dataEU[channel] *= 0.0
+                cleanFile.write("%s " % channel)
 
-        for file in glob.glob(os.path.join(self.dataDir,'*.obc')):
-            head, tail = os.path.split(file)
-            datafiles.append(tail)
-
-        return datafiles
-
-
-    def OnMrgDirPick(self, evt):
-        self.mrgDir = self.mrgdirpick.GetPath()
-
+        self.runObj.dataEU.to_csv(cleanFile, index=False, header=False, sep=' ', float_format='%12.7e')
+        
+        
+        cleanFile.close()
+        
+        
+               
+    def OnAddClick(self, evt):
+        """
+            Add a channel to the output mapping list
+        """
+        
+        # Get the information from the channel boxes
+        orgName = self.chanList.GetString(self.chanList.GetSelection())
+        newName = self.newName.GetLineText(0)
+        newScale = self.newScale.GetLineText(0)
+        
+        self.mapInfo.write("%s, %s, %s \n" % (orgName, newName, newScale))
+        
+        
+        
